77aa31d4936a144f580da1463e8445eb7051699e
ThomasvLingen <thovali96@gmail.com>
2017-01-18 12:57:00 +0100

    initial commit
    Now with const correct main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <iostream>
+
+const int status_code = 0;
+const std::string greeting_message = "Hi";
+
+int main(const int argc, const char** argv)
+{
+    std::cout << greeting_message << std::endl;
+
+    return status_code;
+}
